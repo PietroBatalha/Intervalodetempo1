9261248c9328423f591895a79e8202c6c9c08df5
PietroBatalha <169068274+PietroBatalha@users.noreply.github.com>
2024-09-02 12:03:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.uti.Scanner;
+
+
+public class Main {
+  public static void main(String[] args) {
+  Scanner sc = new scanner(System.in);
+    
+    
+    System.out.println("Hello world!");
+ 
+  int quant(idade)
+  }
+
+
+}
